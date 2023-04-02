b2d7544223ed8b77330623df1fb0b01f33b9b604
sardorbek-matniyazov <sardorbekmatniyazov03@gmail.com>
2023-04-02 17:05:05 +0500

    client update and delete

--- src/main/java/khantorecrm/payload/dto/ClientDto.java
+++ src/main/java/khantorecrm/payload/dto/ClientDto.java
@@ -4,8 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
